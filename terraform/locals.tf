6d06c2d1c7d755f549e55173c2fc74c488341fba
Frank Smith <frankie@ibm.com>
2025-02-19 11:54:16 -0600

    update files

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,84 +1,88 @@
 locals {
-    input_yaml = yamldecode(file("deployment.yaml"))
+  input_yaml = yamldecode(file("deployment.yaml"))
+  handler_ref = try(local.input_yaml["lambda_automation"])
+  handler_selection = local.handler_ref != "FULL_AUTO" ? "handler-automatic-close-and-merge.py" : (local.handler_ref == "AUTO_CLOSE" ? "handler-automatic-close.py" : (local.handler_ref == "APPROVE_ONLY" ? "handler.py" : null ))
 
-    # Name Construction
-    # -----------------
+  # accepted modes are FULL_AUTO, AUTO_CLOSE, APPROVE_ONLY
 
-    customer_prefix = try(local.input_yaml["customer_prefix"])
-    project_uid = try(local.input_yaml["project_uid"])
-    resource_prefix = "${local.customer_prefix}-${local.project_uid}"
+  # Name Construction
+  # -----------------
 
-    # List Building
-    # -------------
-    codecommit = try(local.input_yaml["codecommit"], {})
-    iam = try(local.input_yaml["iam"], {})
+  customer_prefix = try(local.input_yaml["customer_prefix"])
+  project_uid     = try(local.input_yaml["project_uid"])
+  resource_prefix = "${local.customer_prefix}-${local.project_uid}"
 
-    # >>>> Repositories
-    repositories_deploy = try(flatten([
-        for val in local.codecommit["repositories"] : [
-                for approvals in val["approval_templates"] : {
-                    id                 = val["id"]
-                    name               = val["id"]
-                    reference_existing = val["reference_existing"]
-                    approval_templates = val["approval_templates"]
-                } if tostring(val["reference_existing"]) == "false"
-            ]
-        ]   
-    ), {})
+  # List Building
+  # -------------
+  codecommit = try(local.input_yaml["codecommit"], {})
+  iam        = try(local.input_yaml["iam"], {})
 
-    repositories_existing = try(flatten([
-        for val in local.codecommit["repositories"] : [
-            for approvals in val["approval_templates"] : {
-                id                 = val["id"]
-                name               = val["id"]
-                reference_existing = val["reference_existing"]
-            } if tostring(val["reference_existing"]) == "true"
-        ]
-    ]   
-    ), {})
-
-    # >>>> Approval Templates
-    approval_templates_deploy = try(flatten([
-        for val in local.codecommit["approval_templates"] : [
-            for mem in val["pool_members"] : {
-                id                      = val["id"]
-                name                    = val["id"]
-                reference_existing      = val["reference_existing"]
-                approvals_needed        = val["approvals_needed"]
-                pool_members            = val["pool_members"]
-                automated_reviews       = val["automated_reviews"]
-            } if tostring(val["reference_existing"]) == "false"
-        ]  
+  # >>>> Repositories
+  repositories_deploy = try(flatten([
+    for val in local.codecommit["repositories"] : [
+      for approvals in val["approval_templates"] : {
+        id                 = val["id"]
+        name               = val["id"]
+        reference_existing = val["reference_existing"]
+        approval_templates = val["approval_templates"]
+      } if tostring(val["reference_existing"]) == "false"
     ]
-    ), {})
+    ]
+  ), {})
 
-    approval_templates_existing = try(flatten([
-        for val in local.codecommit["approval_templates"] : {
-                    id                 = val["id"]
-                    name               = val["id"]
-                    reference_existing = val["reference_existing"]
-                } if tostring(val["reference_existing"]) == "true"
-            ]   
-    ), {})
+  repositories_existing = try(flatten([
+    for val in local.codecommit["repositories"] : [
+      for approvals in val["approval_templates"] : {
+        id                 = val["id"]
+        name               = val["id"]
+        reference_existing = val["reference_existing"]
+      } if tostring(val["reference_existing"]) == "true"
+    ]
+    ]
+  ), {})
 
-    # >>>> IAM Assumed Roles 
-    assumed_roles_deploy = try(flatten([
-        for val in local.iam["assumed_roles"] : {
-                    id                 = val["id"]
-                    name               = val["id"]
-                    reference_existing = val["reference_existing"]
-                } if tostring(val["reference_existing"]) == "false"
-            ]   
-    ), {})
+  # >>>> Approval Templates
+  approval_templates_deploy = try(flatten([
+    for val in local.codecommit["approval_templates"] : [
+      for mem in val["pool_members"] : {
+        id                 = val["id"]
+        name               = val["id"]
+        reference_existing = val["reference_existing"]
+        approvals_needed   = val["approvals_needed"]
+        pool_members       = val["pool_members"]
+        automated_reviews  = val["automated_reviews"]
+      } if tostring(val["reference_existing"]) == "false"
+    ]
+    ]
+  ), {})
 
-    assumed_roles_existing = try(flatten([
-        for val in local.iam["assumed_roles"] : {
-                    id                 = val["id"]
-                    name               = val["id"]
-                    reference_existing = val["reference_existing"]
-                } if tostring(val["reference_existing"]) == "true"
-            ]   
-    ), {})
+  approval_templates_existing = try(flatten([
+    for val in local.codecommit["approval_templates"] : {
+      id                 = val["id"]
+      name               = val["id"]
+      reference_existing = val["reference_existing"]
+    } if tostring(val["reference_existing"]) == "true"
+    ]
+  ), {})
 
-    # <<<<<<< END OF LOCALS
+  # >>>> IAM Assumed Roles 
+  assumed_roles_deploy = try(flatten([
+    for val in local.iam["assumed_roles"] : {
+      id                 = val["id"]
+      name               = val["id"]
+      reference_existing = val["reference_existing"]
+    } if tostring(val["reference_existing"]) == "false"
+    ]
+  ), {})
+
+  assumed_roles_existing = try(flatten([
+    for val in local.iam["assumed_roles"] : {
+      id                 = val["id"]
+      name               = val["id"]
+      reference_existing = val["reference_existing"]
+    } if tostring(val["reference_existing"]) == "true"
+    ]
+  ), {})
+
+  # <<<<<<< END OF LOCALS
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,178 +1,282 @@
-# codecommit repo
+#       ---------------------------------------------
+#   ▪  ▄▄▄▄· • ▌ ▄ ·.     .▄▄ ·  ▄▄▄·      ▄▄▄  ▄▄▄▄▄.▄▄ · 
+#   ██ ▐█ ▀█▪·██ ▐███▪    ▐█ ▀. ▐█ ▄█▪     ▀▄ █·•██  ▐█ ▀. 
+#   ▐█·▐█▀▀█▄▐█ ▌▐▌▐█·    ▄▀▀▀█▄ ██▀· ▄█▀▄ ▐▀▀▄  ▐█.▪▄▀▀▀█▄
+#   ▐█▌██▄▪▐███ ██▌▐█▌    ▐█▄▪▐█▐█▪·•▐█▌.▐▌▐█•█▌ ▐█▌·▐█▄▪▐█
+#   ▀▀▀·▀▀▀▀ ▀▀  █▪▀▀▀     ▀▀▀▀ .▀    ▀█▄▀▪.▀  ▀ ▀▀▀  ▀▀▀▀ 
+#       ---------------------------------------------
+## >> .summary
+# builds out multiple AWS codecommit repositories with 2 stage codebuild and a single 
+# shared lambda function for enabling CICD automation in codecommit.
 
+# codecommit repo
 resource "aws_codecommit_repository" "this" {
-    for_each        = { for entry in local.repositories_deploy : "${entry.id}" => entry }
-    repository_name = "${local.resource_prefix}-${each.value.name}"
-    description     = "${each.value.name} repository"
+  for_each        = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  repository_name = "${local.resource_prefix}-${each.value.name}"
+  description     = "${each.value.name} repository"
 }
 
 # codecommit repo Rules
 resource "aws_codecommit_approval_rule_template" "automatic" {
-    name        = "lambda_automatic_approval"
-    description = "approval rule template for lambda function "
+  name        = "lambda_automatic_approval"
+  description = "approval rule template for lambda function "
 
-    content = jsonencode({
-        Version               = "2018-11-08"
-        DestinationReferences = ["refs/heads/master"]
-        Statements = [{
-        Type                    = "Approvers"
-        NumberOfApprovalsNeeded = 1
-        ApprovalPoolMembers     = aws_iam_role.lambda.arn #lambda role
-        }]
-    })
+  content = jsonencode({
+    Version               = "2018-11-08"
+    DestinationReferences = ["refs/heads/master"]
+    Statements = [{
+      Type                    = "Approvers"
+      NumberOfApprovalsNeeded = 1
+      ApprovalPoolMembers     = aws_iam_role.cicd.arn #lambda role
+    }]
+  })
 }
 
 resource "aws_codecommit_approval_rule_template" "reviewers" {
-    for_each    = { for entry in local.approval_templates_deploy : "${entry.id}" => entry }
-    name        = each.value.name
-    description = "additional approval rule template for use across multiple repositories"
+  for_each    = { for entry in local.approval_templates_deploy : "${entry.id}" => entry }
+  name        = each.value.name
+  description = "additional approval rule template for use across multiple repositories"
 
-    content = jsonencode({
-        Version               = "2018-11-08"
-        DestinationReferences = ["refs/heads/master"]
-        Statements = [{
-        Type                    = "Approvers"
-        NumberOfApprovalsNeeded = each.value.approvals_needed
-        ApprovalPoolMembers     = each.value.pool_members
-        }]
-    })
+  content = jsonencode({
+    Version               = "2018-11-08"
+    DestinationReferences = ["refs/heads/master"]
+    Statements = [{
+      Type                    = "Approvers"
+      NumberOfApprovalsNeeded = each.value.approvals_needed
+      ApprovalPoolMembers     = each.value.pool_members
+    }]
+  })
 }
 
 # Parsing for Codecommit 
 locals {
-    repositories = values(aws_codecommit_repository.this)[*].repository_name
-    approval_templates_reviewers = values(aws_codecommit_approval_rule_template.reviewers)[*].name
-    approval_templates = concat(local.approval_templates_reviewers, [tostring(aws_codecommit_approval_rule_template.automatic.name)])
-    template_associations_input = setproduct(local.repositories, local.approval_templates)
-    template_associations = [
-        for item in local.template_associations_input : merge({repo = item[0]}, {template = item[1]})
-    ]
+  repositories                 = values(aws_codecommit_repository.this)[*].repository_name
+  approval_templates_reviewers = values(aws_codecommit_approval_rule_template.reviewers)[*].name
+  approval_templates           = concat(local.approval_templates_reviewers, [tostring(aws_codecommit_approval_rule_template.automatic.name)])
+  template_associations_input  = setproduct(local.repositories, local.approval_templates)
+  template_associations = [
+    for item in local.template_associations_input : merge({ repo = item[0] }, { template = item[1] })
+  ]
 }
 
 resource "aws_codecommit_approval_rule_template_association" "this" {
-    for_each                    = { for i in local.template_associations : "${i.repo}-${i.template}" =>  i } 
-    approval_rule_template_name = each.value.repo
-    repository_name             = each.value.template
+  for_each                    = { for i in local.template_associations : "${i.repo}-${i.template}" => i }
+  approval_rule_template_name = each.value.repo
+  repository_name             = each.value.template
 }
 
 # codecommit IAM roles
 resource "aws_iam_role" "this" {
-    for_each           = { for entry in local.assumed_roles_deploy : "${entry.id}" => entry }
-    name               = each.value.name
-    assume_role_policy = data.aws_iam_policy_document.this[each.value.id].json
+  for_each           = { for entry in local.assumed_roles_deploy : "${entry.id}" => entry }
+  name               = each.value.name
+  assume_role_policy = data.aws_iam_policy_document.this[each.value.id].json
 }
 
 data "aws_iam_policy_document" "this" {
-    for_each           = { for entry in local.assumed_roles_deploy : "${entry.id}" => entry }
-    statement {
-        effect = "Allow"
-        principals {
-        type        = "Service"
-        identifiers = ["lambda.amazonaws.com"]
-        }
-        actions = ["sts:AssumeRole"]
+  for_each = { for entry in local.assumed_roles_deploy : "${entry.id}" => entry }
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-resource "aws_iam_role" "lambda" {
-    name               = "iam_for_lambda"
-    assume_role_policy = data.aws_iam_policy_document.lambda.json
+resource "aws_iam_role" "cicd" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.cicd.json
 }
 
-data "aws_iam_policy_document" "lambda" {
-    statement {
-        effect = "Allow"
-        principals {
-        type        = "Service"
-        identifiers = ["lambda.amazonaws.com"]
-        }
-        actions = ["sts:AssumeRole"]
+data "aws_iam_policy_document" "cicd" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# codebuild - test execution environment triggers
+resource "aws_cloudwatch_event_rule" "pr" {
+  for_each    = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  name        = "${each.value.name}-codebuild-pr-rule"
+  description = "initiates test execution environment on PR merge"
+
+  event_pattern = jsonencode({
+    resources = [tostring(aws_codecommit_repository.this["${each.value.id}"].arn)]
+    detail = {
+      event = ["pullRequestCreated", "pullRequestSourceBranchUpdated"]
+    }
+    source = ["aws.codecommit"]
+  })
+  role_arn = aws_iam_role.cicd.arn
+}
+# {
+#   "resources": ["arn:aws:codecommit:eu-central-1:181579366464:ferrari-infra-testing"],
+#   "detail": {
+#     "event": 
+#   },
+#   "source": ["aws.codecommit"]
+# }
+
+resource "aws_cloudwatch_event_target" "pr" {
+  for_each = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  rule     = aws_cloudwatch_event_rule.pr["${each.value.id}"].name
+  arn      = aws_codebuild_project.pr["${each.value.id}"].arn
 }
 
 # codebuild - test phase
-resource "aws_codebuild_project" "this" {
-    name          = "test-project"
-    description   = "test_codebuild_project"
-    build_timeout = 5
-    service_role  = aws_iam_role.lambda.arn #should be the lambda ARN for automation
+resource "aws_codebuild_project" "pr" {
+  for_each      = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  name          = "${each.value.name}-pr"
+  description   = "${each.value.name} pr execution environment"
+  build_timeout = 5
+  service_role  = aws_iam_role.cicd.arn #should be the lambda ARN for automation
 
-    artifacts {
-        type = "NO_ARTIFACTS"
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
     }
+  }
 
-    cache {
-        type     = "S3"
-        location = aws_s3_bucket.example.bucket
-    }
+  source {
+    type            = "CODECOMMIT"
+    location        = aws_codecommit_repository.this["${each.value.id}"].clone_url_http
+    git_clone_depth = 1
+  }
 
-    environment {
-        compute_type                = "BUILD_GENERAL1_SMALL"
-        image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-        type                        = "LINUX_CONTAINER"
-        image_pull_credentials_type = "CODEBUILD"
+  source_version = "main"
 
-        environment_variable {
-        name  = "SOME_KEY2"
-        value = "SOME_VALUE2"
-        type  = "PARAMETER_STORE"
-        }
-    }
-
-    logs_config {
-        cloudwatch_logs {
-        group_name  = "log-group"
-        stream_name = "log-stream"
-        }
-    }
-
-    source {
-        type            = "GITHUB"
-        location        = "https://github.com/mitchellh/packer.git"
-        git_clone_depth = 1
-
-        git_submodules_config {
-        fetch_submodules = true
-        }
-    }
-
-    source_version = "master"
-
-
-
-    tags = {
-        Environment = "Test"
-    }
 }
 
-# codebuild - test phase event triggers
+# codebuild - lambda execution triggers
+resource "aws_cloudwatch_event_rule" "lambda" {
+  for_each = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  name     = "${each.value.name}-lamda-execution"
 
+  description = "initiates lambda function on codebuild test execution outcome"
 
-# codebuild - build phase
-
-
-# codebuild - build phase event triggers
-
+  event_pattern = jsonencode({
+    detail-type = ["CodeBuild Build State Change"],
+    source      = ["aws.codebuild"],
+    detail = {
+      project-name = [tostring(aws_codebuild_project.pr["${each.value.id}"].name)]
+    }
+  })
+  role_arn = aws_iam_role.cicd.arn
+}
+resource "aws_cloudwatch_event_target" "lambda" {
+  for_each = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  rule     = aws_cloudwatch_event_rule.lambda["${each.value.id}"].id
+  arn      = aws_lambda_function.lambda.arn
+  input_transformer {
+    input_paths = {
+      detail-destinationCommit  = "$.detail.destinationCommit",
+      detail-pullRequestId      = "$.detail.pullRequestId",
+      detail-repositoryNames-0- = "$.detail.repositoryNames[0]",
+      detail-revisionId         = "$.detail.revisionId",
+      detail-sourceCommit       = "$.detail.sourceCommit"
+    }
+    input_template = <<EOF
+{"sourceIdentifier":<detail-repositoryNames-0->,"sourceVersion":<detail-sourceCommit>,"artifactsOverride":{"type":"NO_ARTIFACTS"},"environmentVariablesOverride":[{"name":"PULL_REQUEST_ID","value":<detail-pullRequestId>,"type":"PLAINTEXT"},{"name":"REPOSITORY_NAME","value":<detail-repositoryNames-0->,"type":"PLAINTEXT"},{"name":"SOURCE_COMMIT","value":<detail-sourceCommit>,"type":"PLAINTEXT"},{"name":"DESTINATION_COMMIT","value":<detail-destinationCommit>,"type":"PLAINTEXT"},{"name":"REVISION_ID","value":<detail-revisionId>,"type":"PLAINTEXT"}]}
+EOF 
+  }
+}
 
 # lambda function
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "lambda_publish_build_result/handler.py"
+  source_file = "../lambda_publish_build_result/${local.handler_selection}"
   output_path = "lambda_function_payload.zip"
 }
 resource "aws_lambda_function" "lambda" {
-    # If the file is not in the current working directory you will need to include a
-    # path.module in the filename.
-    filename      = "lambda_function_payload.zip"
-    function_name = "${local.resource_prefix}-PullRequestAutomation"
-    role          = aws_iam_role.lambda.arn
-    handler       = "handler.py"
+  filename      = "lambda_function_payload.zip"
+  function_name = "${local.resource_prefix}-PullRequestAutomation"
+  role          = aws_iam_role.cicd.arn
+  handler       = "handler.py"
 
-    runtime = "python3.7"
+  runtime = "python3.7"
 
-    # environment {
-    #     variables = {
-    #         foo = "bar"
-    #     }
-    # }
+}
+
+# codebuild - build exeuction environment triggers #TODO
+resource "aws_cloudwatch_event_rule" "build" {
+  for_each    = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  name        = "${each.value.name}-build"
+  description = "initiates build execution environment on PR merge"
+
+  event_pattern = jsonencode({
+    resources = [tostring(aws_codecommit_repository.this["${each.value.id}"].arn)]
+    detail = {
+      event = ["pullRequestMergeStatusUpdated"],
+      isMerged = ["True"]
+    }
+    source = ["aws.codecommit"]
+  })
+  role_arn = aws_iam_role.cicd.arn
+}
+
+resource "aws_cloudwatch_event_target" "build" {
+  for_each = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  rule     = aws_cloudwatch_event_rule.build["${each.value.id}"].name
+  arn      = aws_codebuild_project.build["${each.value.id}"].arn
+}
+
+# codebuild - build phase
+resource "aws_codebuild_project" "build" {
+  for_each      = { for entry in local.repositories_deploy : "${entry.id}" => entry }
+  name          = "${each.value.name}-build"
+  description   = "${each.value.name} build execution environment"
+  build_timeout = 5
+  service_role  = aws_iam_role.cicd.arn #should be the lambda ARN for automation
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "SOME_KEY2"
+      value = "SOME_VALUE2"
+      type  = "PARAMETER_STORE"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type            = "CODECOMMIT"
+    location        = aws_codecommit_repository.this["${each.value.id}"].clone_url_http
+    git_clone_depth = 1
+  }
+
+  source_version = "main"
+
 }
